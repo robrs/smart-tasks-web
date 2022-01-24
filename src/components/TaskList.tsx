ae9587d8c1c0f0a133a81e80296575b881d7621a
Roberto Rocha <robrs.web@gmail.com>
2022-01-24 13:26:20 -0300

    ajuste no header

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -25,7 +25,6 @@ export function TaskList() {
     
     tasks.push(newTask);
     setTasks(tasks);
-    // setTasks((oldState) => [...oldState, newTask]);
     setNewTaskTitle("");
   }
 
@@ -49,12 +48,12 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minhas tarefas</h2>
 
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Adicionar novo todo" 
+            placeholder="Adicionar nova tarefa" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
